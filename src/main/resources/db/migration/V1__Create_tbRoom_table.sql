49034ace35c5100f4d2ab0d87075a1d78985794d
Matheus Nascimento <matheusmn1519@gmail.com>
2024-09-06 00:41:17 -0300

    add authentication

--- src/main/resources/db/migration/V1__Create_tbRoom_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tb_room (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    room_number VARCHAR(255) NOT NULL,
-    room_type VARCHAR(255),
-    capacity INT,
-    price_per_night DOUBLE,
-    available BOOLEAN,
-    PRIMARY KEY (id)
-);
